ee83b94a98d7cf83031902e56ad99f3e1b314f32
minhdevs <104881854+minhdevs@users.noreply.github.com>
2024-03-29 15:34:46 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/poly_truyen_client/MainActivity.java
+++ app/src/main/java/com/example/poly_truyen_client/MainActivity.java
@@ -42,76 +42,7 @@ import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
 
 public class MainActivity extends AppCompatActivity {
-    private Socket socket;
-    private ActivityMainBinding binding;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        new SocketConfig();
-        SocketManager socketManager = SocketManager.getInstance(new ConnectAPI().API_URL);
-        socket = socketManager.getSocket();
-        socket.on("ServerPostNewComic", new Emitter.Listener() {
-            @Override
-            public void call(Object... args) {
-                MainActivity.this.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        notifi("Poly comic đã đăng một truyện mới!", args[0].toString());
-
-                        EventBus.getDefault().post(new NotificationEvent());
-
-                    }
-                });
-            }
-        });
-
-
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_history,
-                R.id.navigation_home,  R.id.navigation_settings)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
-    }
-
-    void notifi(String title, String comicData){
-        Comic comic = new Gson().fromJson(comicData, Comic.class);
-
-        Intent intent = new Intent(MainActivity.this, ViewDetailsComicActivity.class);
-        intent.putExtra("comic", comicData);
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
-
-        Notification customNotification = new NotificationCompat.Builder(MainActivity.this, NotifyConfig.CHANEL_ID)
-                .setSmallIcon(R.drawable.icon)
-                .setContentTitle(title)
-                .setContentText(comic.getName())
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true)
-                .build();
-
-        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(MainActivity.this);
-
-        if (ActivityCompat.checkSelfPermission(MainActivity.this,
-                android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-
-            // Gọi hộp thoại hiển thị xin quyền người dùng
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, 999999);
-            Toast.makeText(MainActivity.this, "Vui lòng cấp quyền gửi thông báo!", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        int id_notiy = (int) new Date().getTime();
-        notificationManagerCompat.notify(id_notiy , customNotification);
+    
 
     }
 
